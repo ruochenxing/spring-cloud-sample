abb33ca1f48063366ee05a8204d092713a845026
zdd <sherlock_65535@qq.com>
2018-06-10 13:53:15 +0800

    add config

--- spring-cloud-config-client/src/main/java/net/zxjava/Application.java
+++ spring-cloud-config-client/src/main/java/net/zxjava/Application.java
@@ -2,12 +2,10 @@ package net.zxjava;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class).run(args);
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
 	}
 }
