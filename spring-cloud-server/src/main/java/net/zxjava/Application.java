11b73db49713bb51976165d0fcf44176b8cb3cd9
zdd <sherlock_65535@qq.com>
2018-06-10 16:22:00 +0800

    add sleuth

--- spring-cloud-server/src/main/java/net/zxjava/Application.java
+++ spring-cloud-server/src/main/java/net/zxjava/Application.java
@@ -1,7 +1,7 @@
 package net.zxjava;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class Application {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class).web(true).run(args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
