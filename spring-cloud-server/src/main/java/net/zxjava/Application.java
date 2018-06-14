3fee0ce88fa9fb7aab96e18d5807204dde7485f4
zdd <sherlock_65535@qq.com>
2018-06-14 18:11:52 +0800

    finished server

--- spring-cloud-server/src/main/java/net/zxjava/Application.java
+++ spring-cloud-server/src/main/java/net/zxjava/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// java -jar target/spring-cloud-server-1.0-RELEASE.jar --spring.profiles.active=slave1
 @SpringBootApplication
 @EnableEurekaServer
 public class Application {
