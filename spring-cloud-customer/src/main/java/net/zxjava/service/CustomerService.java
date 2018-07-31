a3b26f733a145a2d444485cdba84a4fa5d8e71a9
zdd <sherlock_65535@qq.com>
2018-07-31 10:24:12 +0800

    optimized code

--- spring-cloud-customer/src/main/java/net/zxjava/service/CustomerService.java
+++ spring-cloud-customer/src/main/java/net/zxjava/service/CustomerService.java
@@ -1,11 +1,27 @@
 package net.zxjava.service;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("SPRING-CLOUD-SERVICE")
 public interface CustomerService {
-	
-	@GetMapping("/hello")
+
+	@RequestMapping("/hello")
 	public String hello();
+
+	// http://xxxxx?name=xxxxxx
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public String user(@RequestParam(value = "name", required = true) String name);
+
+	// http://xxxxx/1111 id=1111
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public String getUser(@PathVariable("id") Integer id);
+
+	// post json
+	@RequestMapping(value = "/createUser", method = RequestMethod.POST)
+	public String createUser(@RequestBody Object user);
 }
