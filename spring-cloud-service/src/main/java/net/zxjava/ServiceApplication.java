23d834f77f1668c3d2ab3e0309dc98b3047164bb
zdd <sherlock_65535@qq.com>
2018-08-02 16:17:25 +0800

    optimized code

--- spring-cloud-service/src/main/java/net/zxjava/ServiceApplication.java
+++ spring-cloud-service/src/main/java/net/zxjava/ServiceApplication.java
@@ -2,10 +2,10 @@ package net.zxjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient	//@EnableDiscoveryClient
 public class ServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
