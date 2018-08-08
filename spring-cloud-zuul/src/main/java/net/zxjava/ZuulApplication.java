971297cf78343890679647c4c3611a2abc220c28
zdd <sherlock_65535@qq.com>
2018-08-08 15:35:41 +0800

    optimized code

--- spring-cloud-zuul/src/main/java/net/zxjava/ZuulApplication.java
+++ spring-cloud-zuul/src/main/java/net/zxjava/ZuulApplication.java
@@ -3,35 +3,27 @@ package net.zxjava;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-
-import com.netflix.zuul.FilterProcessor;
-
-import net.zxjava.ext.MyFilterProcessor;
-import net.zxjava.filter.AccessFilter;
-import net.zxjava.filter.ErrorFilter;
-import net.zxjava.filter.ThrowExceptionFilter;
 
 @EnableZuulProxy
 @SpringCloudApplication
 public class ZuulApplication {
 	public static void main(String[] args) {
-		FilterProcessor.setProcessor(new MyFilterProcessor());
+		// FilterProcessor.setProcessor(new MyFilterProcessor());
 		new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
 	}
 
-	@Bean
-	public AccessFilter accessFilter() {
-		return new AccessFilter();
-	}
-
-	@Bean
-	public ThrowExceptionFilter throwExceptionFilter() {
-		return new ThrowExceptionFilter();
-	}
-
-	@Bean
-	public ErrorFilter errorFilter() {
-		return new ErrorFilter();
-	}
+	// @Bean
+	// public AccessFilter accessFilter() {
+	// return new AccessFilter();
+	// }
+	//
+	// @Bean
+	// public ThrowExceptionFilter throwExceptionFilter() {
+	// return new ThrowExceptionFilter();
+	// }
+	//
+	// @Bean
+	// public ErrorFilter errorFilter() {
+	// return new ErrorFilter();
+	// }
 }
