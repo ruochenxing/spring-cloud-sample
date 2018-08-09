dcfd240f2e27d1c87e22d6856987e4733a211554
zdd <sherlock_65535@qq.com>
2018-08-09 16:28:05 +0800

    optimized code

--- spring-cloud-zuul/src/main/java/net/zxjava/ZuulApplication.java
+++ spring-cloud-zuul/src/main/java/net/zxjava/ZuulApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringCloudApplication
 public class ZuulApplication {
 	public static void main(String[] args) {
-		// FilterProcessor.setProcessor(new MyFilterProcessor());
 		new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
 	}
 
