f826d72e20146487e79eb1d2d2d76d987acf77d5
zdd <sherlock_65535@qq.com>
2018-08-09 14:20:06 +0800

    finished

--- spring-cloud-zuul/src/main/java/net/zxjava/ZuulApplication.java
+++ spring-cloud-zuul/src/main/java/net/zxjava/ZuulApplication.java
@@ -8,7 +8,12 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringCloudApplication
 public class ZuulApplication {
 	public static void main(String[] args) {
-//		FilterProcessor.setProcessor(new MyFilterProcessor());
+		// FilterProcessor.setProcessor(new MyFilterProcessor());
 		new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
 	}
+
+//	@Bean
+//	public PatternServiceRouteMapper serviceRouteMapper() {
+//		return new PatternServiceRouteMapper("(?<name>^.+)-(?<version>v.+$)", "${version}/${name}");
+//	}
 }
---
+++ spring-cloud-zuul/src/main/java/net/zxjava/ext/MyRouteLocator.java
@@ -0,0 +1,58 @@
+package net.zxjava.ext;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.cloud.netflix.zuul.filters.RefreshableRouteLocator;
+import org.springframework.cloud.netflix.zuul.filters.SimpleRouteLocator;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties.ZuulRoute;
+import org.springframework.util.StringUtils;
+
+//@Component
+public class MyRouteLocator extends SimpleRouteLocator implements RefreshableRouteLocator {
+
+	private ZuulProperties properties;
+
+	public MyRouteLocator(String servletPath, ZuulProperties properties) {
+		super(servletPath, properties);
+		this.properties = properties;
+	}
+
+	@Override
+	public void refresh() {
+		doRefresh();
+	}
+
+	@Override
+	protected Map<String, ZuulRoute> locateRoutes() {
+		LinkedHashMap<String, ZuulRoute> routesMap = new LinkedHashMap<String, ZuulRoute>();
+		// 从application.properties中加载路由信息
+		routesMap.putAll(super.locateRoutes());
+		// 从中加载路由信息
+		routesMap.putAll(locateRoutesFromOther());
+		// 优化一下配置
+		LinkedHashMap<String, ZuulRoute> values = new LinkedHashMap<>();
+		for (Map.Entry<String, ZuulRoute> entry : routesMap.entrySet()) {
+			String path = entry.getKey();
+			// Prepend with slash if not already present.
+			if (!path.startsWith("/")) {
+				path = "/" + path;
+			}
+			if (StringUtils.hasText(this.properties.getPrefix())) {
+				path = this.properties.getPrefix() + path;
+				if (!path.startsWith("/")) {
+					path = "/" + path;
+				}
+			}
+			values.put(path, entry.getValue());
+		}
+		return values;
+	}
+
+	private Map<String, ZuulRoute> locateRoutesFromOther() {
+		Map<String, ZuulRoute> routes = new LinkedHashMap<>();
+		// TODO
+		return routes;
+	}
+}
